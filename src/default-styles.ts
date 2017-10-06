5ed23e4544b056677ed315857704cc9579f2cb0f
Bas Doppen <basdoppen@Bass-MacBook.local>
2017-10-06 09:02:27 +0200

    change font weight

--- src/default-styles.ts
+++ src/default-styles.ts
@@ -3,6 +3,7 @@ import * as React from 'react';
 const fontStyle: React.CSSProperties = {
     fontFamily: "'Roboto', sans-serif",
     fontSize: '18px',
+	fontWeight: 300,
 }
 
 export { fontStyle };
