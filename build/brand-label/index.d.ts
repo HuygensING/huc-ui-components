3f757e1717ce51c1f5111559844a5b24d7d9f63d
Bas Doppen <accounts@neppod.com>
2017-11-13 17:05:35 +0100

    Component: Brand label

---
+++ build/brand-label/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+declare const levisLabel: () => JSX.Element;
+export default levisLabel;
---
+++ build/brand-label/index.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
+const levisLabel = () => React.createElement("div", { style: {
+        position: 'fixed',
+        right: '0',
+        bottom: '80px',
+        margin: -right, '-35px': ,
+        border: -radius, '2px 0px 2px 0px': ,
+        background: -color, '#268f75': ,
+        color: '#fff',
+        padding: '0em 1em',
+        font: -size, '.6em': ,
+        transform: 'rotate(90deg)',
+    } },
+    "Powered by",
+    React.createElement("img", { style: {
+            height: 'auto',
+            width: '17px',
+            margin: '-5px 0 -5px 0',
+        }, src: "http://design.huygens.knaw.nl/wp-content/themes/huc-design-system/images/logo-timbuctoo-inv.svg", alt: "Pergamon" }));
+exports.default = levisLabel;
