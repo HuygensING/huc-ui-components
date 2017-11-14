5378b5c2528e2f229a91dd6eb94adabaa712898c
Gijsjan <agijsbro@gmail.com>
2017-11-14 11:39:53 +0100

    Fix React.CSSProperties

--- src/brand-label/index.tsx
+++ src/brand-label/index.tsx
@@ -6,24 +6,25 @@ const levisLabel = () =>
       position: 'fixed',
       right: '0',
       bottom: '80px',
-      margin-right: '-35px',
-      border-radius: '2px 0px 2px 0px',
-      background-color: '#268f75',
+      marginRight: '-35px',
+      borderRadius: '2px 0px 2px 0px',
+      backgroundColor: '#268f75',
       color: '#fff',
       padding: '0em 1em',
-      font-size: '.6em',
+      fontSize: '.6em',
       transform: 'rotate(90deg)',
 		}}
 	>
     Powered by
     <img
-    style={{
-      height: 'auto',
-      width: '17px',
-      margin: '-5px 0 -5px 0',
-    }}
-    src="http://design.huygens.knaw.nl/wp-content/themes/huc-design-system/images/logo-timbuctoo-inv.svg"
-    alt="Pergamon"/>
+      style={{
+        height: 'auto',
+        width: '17px',
+        margin: '-5px 0 -5px 0',
+      }}
+      src="http://design.huygens.knaw.nl/wp-content/themes/huc-design-system/images/logo-timbuctoo-inv.svg"
+      alt="Pergamon"
+    />
   </div>;
 
 export default levisLabel;
