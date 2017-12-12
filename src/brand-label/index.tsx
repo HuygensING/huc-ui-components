592df4ee1bd4b34e3dd63b365fa8f4657745447d
Bas Doppen <basdoppen@gmail.com>
2017-12-12 09:12:13 +0100

    Levislabel Pergamon

--- src/brand-label/index.tsx
+++ src/brand-label/index.tsx
@@ -17,16 +17,22 @@ const levisLabel = () =>
       transform: 'rotate(90deg)',
 		}}
 	>
-    Powered by
+    <a href="http://it.huygens.knaw.nl/index.php/digital-infrastructure/pergamon/" target="_blanc"
+			style={{
+				color: '#fff',
+				textDecoration: 'none',
+				}}
+		>Powered by
     <img
       style={{
         height: 'auto',
         width: '17px',
-        margin: '-5px 0 -5px 0',
+        margin: '-5px 0 -5px 5px',
+				transform: 'rotate(-90deg)',
       }}
-      src="http://design.huygens.knaw.nl/wp-content/themes/huc-design-system/images/logo-timbuctoo-inv.svg"
+      src="https://design.huygens.knaw.nl/static/logo-pergamon.svg"
       alt="Pergamon"
-    />
+    /></a>
   </div>
 
 export default levisLabel;
