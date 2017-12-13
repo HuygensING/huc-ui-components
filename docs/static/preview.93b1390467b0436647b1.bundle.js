f825c7b11f9c45397690b02419953f9c73ec69f0
Gijsjan <agijsbro@gmail.com>
2017-12-13 21:59:23 +0100

    New build

--- docs/static/preview.93b1390467b0436647b1.bundle.js
+++ docs/static/preview.93b1390467b0436647b1.bundle.js
@@ -40369,26 +40369,36 @@ exports.default = ResultBody;
 
 Object.defineProperty(exports, "__esModule", { value: true });
 const React = __webpack_require__(0);
-const levisLabel = () => React.createElement("div", { style: {
+const levisLabel = (props) => React.createElement("div", { style: {
+        backgroundColor: '#268f75',
+        borderRadius: '2px 0px 2px 0px',
+        bottom: '80px',
+        color: '#fff',
+        fontFamily: "'Roboto', sans-serif",
+        fontSize: '11px',
+        fontWeight: 300,
+        marginRight: '-35px',
+        padding: '.5em 1em',
         position: 'fixed',
         right: '0',
-        bottom: '80px',
-        marginRight: '-35px',
-        borderRadius: '2px 0px 2px 0px',
-        fontFamily: "'Roboto', sans-serif",
-        fontWeight: 300,
-        backgroundColor: '#268f75',
-        color: '#fff',
-        padding: '.5em 1em',
-        fontSize: '11px',
         transform: 'rotate(90deg)',
+        zIndex: 999
     } },
-    "Powered by",
-    React.createElement("img", { style: {
-            height: 'auto',
-            width: '17px',
-            margin: '-5px 0 -5px 0',
-        }, src: "http://design.huygens.knaw.nl/wp-content/themes/huc-design-system/images/logo-timbuctoo-inv.svg", alt: "Pergamon" }));
+    React.createElement("a", { href: props.href, target: "_blanc", style: {
+            color: '#fff',
+            textDecoration: 'none',
+        } },
+        "Powered by",
+        React.createElement("img", { style: {
+                height: 'auto',
+                width: '17px',
+                margin: '-5px 0 -5px 5px',
+                transform: 'rotate(-90deg)',
+            }, src: `${props.imgBaseDir}/logo-pergamon.svg`, alt: "Pergamon" })));
+levisLabel.defaultProps = {
+    href: "http://it.huygens.knaw.nl/index.php/digital-infrastructure/pergamon/",
+    imgBaseDir: '/static/graphics/ui',
+};
 exports.default = levisLabel;
 
 
@@ -42980,4 +42990,4 @@ module.exports = __webpack_require__(544);
 
 /***/ })
 /******/ ]);
-//# sourceMappingURL=preview.b6ba9c31e4612b85b648.bundle.js.map
+//# sourceMappingURL=preview.93b1390467b0436647b1.bundle.js.map
